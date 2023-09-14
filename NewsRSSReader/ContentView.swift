ef02eb4a02fd41e0830797471c1cec343298201f
Andrey Simakov <asimakov@idscan.net>
2023-09-14 14:08:21 +0300

    Main top

--- NewsRSSReader/ContentView.swift
+++ NewsRSSReader/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            HStack{
+                Image(systemName: "line.horizontal.3")
+                    .foregroundColor(Color("White"))
+                    .padding(10)
+                Image("logo")
+                    .resizable()
+                    .frame(width: 120.0, height: 20.0)
+                    .padding(10)
+                Spacer()
+            }.background(Color("Black"))
+            
+            VStack{
+                Spacer()
+                Text("Китай обнародовал план по углублению интеграции с Тайванем")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(Color("White"))
+                    .clipped()
+                HStack{
+                    Text("13:01")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("Gray"))
+                        .padding(.trailing, 10)
+                    Text("Мир")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("Gray"))
+                    Spacer()
+                }.padding()
+            }
+            .background(
+                 Image("background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(
+                        LinearGradient(gradient: Gradient(colors: [.clear, Color("Black")]),
+                                           startPoint: .center,
+                                           endPoint: .bottom)
+                        )
+            )
+            .frame(height: 350)
+            .clipped()
+            //.fixedSize(horizontal: false, vertical: true)
+
+            Spacer()
         }
-        .padding()
     }
 }
 
